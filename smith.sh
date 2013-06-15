b6fb785e234e28ac780a1222e309ab70f8e1f2ce
Swaagie <info@martijnswaagman.nl>
2013-06-16 01:27:56 +0200

    initial

---
+++ smith.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+DIRECTORY='/.git'
+CURRENT=`pwd`
+
+until [ $CURRENT = '/' ]
+do
+  if [ -d "$CURRENT$DIRECTORY" ]; then
+    NAME=$(basename $(git remote show -n origin | grep Fetch | cut -d: -f2-))
+    echo -ne "\033]0; $NAME\007"
+    break
+  fi
+
+  CURRENT=$(dirname $CURRENT)
+done
