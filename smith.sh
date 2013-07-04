234dbc3cd96a3716f26386abfab913d0ed0b1f10
Swaagie <info@martijnswaagman.nl>
2013-07-04 10:40:46 +0200

    [fix] restore title if outside git directory

--- smith.sh
+++ smith.sh
@@ -4,22 +4,36 @@ function gitName {
   DIRECTORY='/.git'
   CURRENT=`pwd`
 
+  #
+  # Recursively run down directories until we find .git or are at `/`
+  #
+  unset TITLE
   until [ $CURRENT = '/' ]
   do
-    if [ -d "$CURRENT$DIRECTORY" ]; then
+    if [ -d "$CURRENT$DIRECTORY" ]
+    then
       NAME=$(basename $(git remote show -n origin | grep Fetch | cut -d: -f2-))
       BRANCH=$(basename $(git symbolic-ref HEAD))
-      echo -ne "\033]0; ${NAME%.git} ($BRANCH)\007"
+      TITLE="\033]0; ${NAME%.git} ($BRANCH)\007"
+
+      echo -ne $TITLE
       break
     fi
 
     CURRENT=$(dirname $CURRENT)
   done
+
+  # 
+  # Restore to a default Terminal title.
+  #
+  if [ -z "$TITLE" ] 
+  then
+    echo -ne "\033]0; Terminal \007"
+  fi
 }
 
 function cd {
   builtin cd "$@"
-
   gitName
 }
 
